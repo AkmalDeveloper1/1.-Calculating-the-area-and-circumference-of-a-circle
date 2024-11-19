6f2a8f048c8d78408c3b55f346baeebb4c12d875
AkmalDeveloper <rahmonovakmal2101@gmail.com>
2024-11-19 23:40:13 +0500

    Calculating the area and circumference of a circle

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// 1. Calculating the area and circumference of a circle
+Console.Write("Enter the radius :");
+decimal radius = Convert.ToDecimal(Console.ReadLine());
+decimal L, S;
+L = 2 * (decimal)Math.PI * radius;
+S = L/2 * radius;
+Console.WriteLine($"S={S}\nL={L}");
